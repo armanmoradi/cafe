0784d0dfc7d316104ddc530fb24c372fd1bf32f8
armanmoradi <armanmoradi8@gmail.com>
2023-01-30 00:45:53 +0330

    use scope for search user name and cafe_restaurant name

--- app/models/cafe_restaurant.rb
+++ app/models/cafe_restaurant.rb
@@ -1,4 +1,6 @@
 class CafeRestaurant < ApplicationRecord
     has_many :user_role
     validates :name, presence: true
+    scope :search_name_cafe, ->(name) { where("name ILIKE ?" , "%#{name}%")}
+    # Ex:- scope :active, -> {where(:active => true)}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
     has_many :user_role
     validates :name, presence: true
+    scope :search_user_name, ->(name) { where("name ILIKE ?", "%#{name}%")}
+    # Ex:- scope :active, -> {where(:active => true)}
 end
