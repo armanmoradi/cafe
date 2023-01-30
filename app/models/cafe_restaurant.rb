d5e446bd01bdb1fd33d63164d70790c22e7c9dcf
armanmoradi <armanmoradi8@gmail.com>
2023-01-30 20:44:27 +0330

    edit validation

--- app/models/cafe_restaurant.rb
+++ app/models/cafe_restaurant.rb
@@ -1,6 +1,6 @@
 class CafeRestaurant < ApplicationRecord
-    has_many :user_role
+    has_many :user_roles, dependent: :destroy
+    has_many :user , through: :user_roles
     validates :name, presence: true
     scope :search_name_cafe, ->(name) { where("name ILIKE ?" , "%#{name}%")}
-    # Ex:- scope :active, -> {where(:active => true)}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-    has_many :user_role
+    has_many :user_roles, dependent: :destroy
+    has_many :cafe_restaurants , through: :user_roles
     validates :name, presence: true
     scope :search_user_name, ->(name) { where("name ILIKE ?", "%#{name}%")}
-    # Ex:- scope :active, -> {where(:active => true)}
 end
